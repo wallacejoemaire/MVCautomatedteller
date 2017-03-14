496898bbb4ae5cea885f96ab5d2cee5fd77356d0
joe wallace <wallacejoemarie@live.com>
2017-03-13 22:03:02 -0600

    test commit

--- automatedteller/Controllers/HomeController.cs
+++ automatedteller/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace automatedteller.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page is great.";
 
             return View();
         }
